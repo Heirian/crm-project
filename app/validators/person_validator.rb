a1eec8c532dbd9be7cd421417487bf44073a7755
Bruno Teófilo Domingos Silva <shinkcc@gmail.com>
2017-10-21 18:23:18 +0000

    Revert "Merge branch 'templateViews' into 'master'"
    This reverts merge request !6

--- app/validators/person_validator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class PersonValidator < ActiveModel::Validator
-  def validate(record)
-    return if options[:fields].any? { |field| record.send(field) == 'Individual' || 'Company' }
-    record.errors[:base] << I18n.t(:type_not_permited)
-  end
-end
--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,9 @@ module Testapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = [:en, 'pt-BR']
-    config.i18n.default_locale = 'pt-BR'
   end
 end
