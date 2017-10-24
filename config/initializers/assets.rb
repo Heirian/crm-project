e91fa30e6893bc98310fd4666eb8e977c249ca5b
Pedro Furtado <pedro.furtado@hotmail.com>
2017-10-24 09:44:25 -0200

    insere menu e tema personalizado

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,12 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( font-awesome.css )
+Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w( jquery-1.10.2.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( custom.js )
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
