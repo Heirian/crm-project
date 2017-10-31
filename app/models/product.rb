245f1ad032c6f8ad270dd3901abc3c371ee2447d
Pedro Furtado <pedro.furtado@hotmail.com>
2017-10-31 17:09:10 -0200

    good and service ok

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,13 +2,13 @@
 # t.text "description"
 # t.string "type"
 # t.integer "stock"
-# t.decimal "baseValue"
+# t.decimal "base_value"
 #t.string "category"
 
 class Product < ApplicationRecord
   acts_as_paranoid
 
-  validates :name, :description, :type, :baseValue, :category, presence: true
+  validates :name, :description, :type, :base_value, :category, presence: true
   validates :name, :description, :category, format: /\A[^`!@#\$%\^&*+_=]+\z/
   validates_with ProductValidator, fields: [:type]
   validates_uniqueness_of :name
