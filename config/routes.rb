6df79a9c9493a131271044b81a85ad9cb96a77b3
Bruno Teófilo Domingos Silva <shinkcc@gmail.com>
2017-11-06 17:04:44 +0000

    Revert "Merge branch 'feature/courses' into 'dev'"
    This reverts merge request !44

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :courses
   resources :products
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106122636) do
+ActiveRecord::Schema.define(version: 20171030193820) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street_address"
@@ -64,14 +64,6 @@ ActiveRecord::Schema.define(version: 20171106122636) do
     t.datetime "updated_at", null: false
     t.index ["deleted_at"], name: "index_countries_on_deleted_at"
     t.index ["name"], name: "index_countries_on_name"
-  end
-
-  create_table "courses", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "courseLoad"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_courses_on_product_id"
   end
 
   create_table "emails", force: :cascade do |t|
